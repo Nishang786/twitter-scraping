5c33b8acbb6f301c869ca47873f32171725cde1f
Nishang Lingalwar <nishang786@gmail.com>
2020-10-16 05:18:03 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -2,10 +2,10 @@ import tweepy
 from tweepy.api import API
 from tweepy.binder import bind_api
 import logging
-consumer_key = 'I4dAcf2EuoNOL41aoEdMzPhTL'
-consumer_secret = 'pIfst3OLbBNDwigBMjpkqMjMTEIAyjG9AF79GlR9vb06k9ikXK'
-access_token = '1310946692397764620-JhNEgW3OW4g2wCMWNvdzwNxGic4EfB'
-access_secret = 'jAirddJvqNtw6VzZXGcnGgC0CalydQvIrDHxlCBwhkvSB'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
